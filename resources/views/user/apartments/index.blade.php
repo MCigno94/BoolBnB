e5773cd3f1a3f605ab527aca945a24c2102b010f
VictorEnache <victor22enache@gmail.com>
2022-07-18 23:04:52 +0200

    change condition

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -5,7 +5,7 @@
 @foreach($apartments as $apartment)
     <h1>{{$apartment->title}}</h1>
 
-    <img src="{{ ($apartment->img = 'Case-moderne.jpg') ? asset('img/Case-moderne.jpg') : asset('storage/' . $apartment->img) }}" alt="">
+    <img src="{{ ($apartment->img === 'Case-moderne.jpg') ? asset('img/Case-moderne.jpg') : asset('storage/' . $apartment->img) }}" alt="">
     <hr>
 @endforeach
 
