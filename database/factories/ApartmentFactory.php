9329d9c862c91ef5f1486402134c917707721b51
VictorEnache <victor22enache@gmail.com>
2022-07-18 15:28:31 +0200

    make:model apartment -a

---
+++ database/factories/ApartmentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\apartment;
+use Faker\Generator as Faker;
+
+$factory->define(apartment::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
