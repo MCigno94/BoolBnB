199c3673383cb836351293c78f5903b4dccaafe4
ariannagreco3393 <a.greco3393@gmail.com>
2022-07-25 16:47:57 +0200

    fix show layout

--- resources/views/user/apartments/show.blade.php
+++ resources/views/user/apartments/show.blade.php
@@ -16,14 +16,14 @@
      <!-- BUTTONS -->
      <div class="d-flex align-items-center justify-content-between">
         <div class="visibility">
-            @if ($apartment->visibility) 
+            @if ($apartment->visibility)
                 {{-- <i class="fas fa-circle mr-2"></i> --}}
                 <span class="mx-3"><i style="color: green" class="fas fa-circle mx-2"></i>Visible</span>
             @else
                 <span class="mx-3"><i style="color: red" class="fas fa-circle mx-2"></i> Not Visible</span>
             @endif
         </div>
-        
+
         <div class="buttons d-flex gap-2">
             <div class="button">
                 <a class="btn btn-light" href="{{route('user.apartments.edit', $apartment->id)}}">
@@ -73,21 +73,35 @@
         </a>
     </div> -->
 
-    <div class="card">
+    <div>
         <h1>{{$apartment->title}}</h1>
         <div class="img_container">
-            
-            <img class="apartment_img" src="{{ ($apartment->img === 'Case-moderne.jpg') ? asset('img/Case-moderne.jpg') : asset('storage/' . $apartment->img) }}" alt="{{$apartment->title}}">
+            <img class="apartment_img" src="{{asset('storage/' . $apartment->img)}}"
+            alt="{{$apartment->title}}">
         </div>
     </div>
-
-    <div class="d-flex justify-content-center">
-    <p class="w-50 text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus excepturi eaque aliquid sit perspiciatis cum quasi omnis molestiae odit blanditiis nisi officia deleniti similique nobis, provident doloremque expedita ullam suscipit veritatis natus maiores? Nisi voluptas inventore temporibus quia eveniet distinctio consequuntur nemo odit, esse, voluptatum facere nostrum voluptate quam necessitatibus dolorum at tenetur. Eos repellendus beatae maiores, iusto laborum, qui autem tenetur ullam, aut velit natus dignissimos labore vitae delectus accusantium cupiditate tempore commodi ipsa voluptatem provident. Ipsa iure, quasi adipisci alias laudantium maiores et obcaecati totam deserunt sit dicta fuga in dignissimos quam quas consectetur. Enim id libero incidunt!</p>
-    <div id="map" style="width: 450px; height: 450px;"></div>
+    <div class="container mt-5">
+        <div class="row d-flex justify-content-around">
+        <div class="col">
+            <h2>Description</h2>
+            <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus excepturi eaque aliquid
+            sit perspiciatis cum quasi omnis molestiae odit blanditiis nisi officia deleniti similique nobis, provident doloremque
+            expedita ullam suscipit veritatis natus maiores? Nisi voluptas inventore temporibus quia eveniet distinctio consequuntur
+            nemo odit, esse, voluptatum facere nostrum voluptate quam necessitatibus dolorum at tenetur. Eos repellendus beatae maiores,
+            iusto laborum, qui autem tenetur ullam, aut velit natus dignissimos labore vitae delectus accusantium cupiditate tempore commodi
+            ipsa voluptatem provident. Ipsa iure, quasi adipisci alias laudantium maiores et obcaecati totam deserunt sit dicta fuga in dignissimos
+            quam quas consectetur. Enim id libero incidunt!
+            </p>
+        </div>
+       <div class="col">
+        <h2>Map</h2>
+            <div id="map" style="width: 500px; height: 300px;"></div>
+       </div>
     </div>
     <div class="mes-host pt-5 d-flex">
         <div class="w-50 ps-3">
-            <h2>Cosa troverai</h2>
+            <h2>Services</h2>
             <div class="p-1">
             @foreach($services as $service)
                         <div class="col">
@@ -100,15 +114,13 @@
                         </div>
              @endforeach
             </div>
-
         </div>
-        <div class="mb-3 w-50">
-          <h3>Invia un messaggio all'host</h3>
-          <input type="text" class="form-control p-5 mt-3" name="" id="" aria-describedby="helpId" placeholder="">
-          <button type="button" class="btn btn-primary mt-3">Button</button>
+        <div>
+          <h3>Send a message</h3>
+          <textarea type="text" cols="60" rows="10" class="form-control mt-3" name="" id="" aria-describedby="helpId" placeholder=""></textarea>
+          <button type="button" class="btn btn-danger mt-3">Send</button>
         </div>
-
     </div>
-
+    </div>
 </section>
 @endsection
